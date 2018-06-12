c7cebc2db829c3ec99470561ff62831907e7f12e
eherrmann <eherrmann@excilys.com>
2018-06-12 17:39:43 +0200

    oubli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CustomMaterialModule} from './custom-material/custom-material.module';
+import { HeaderComponent } from './header/header.component';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './/app-routing.module';
+import {RecipeModule} from './recipe/recipe.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RecipeModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
